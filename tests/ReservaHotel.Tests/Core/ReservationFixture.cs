1f2b95ab68ed11ad3b87b65fd02e919a59d65cab
Matheus-Morais <matheus2992morais@gmail.com>
2023-05-30 15:10:09 -0300

    Adiciona tests para reservation

---
+++ tests/ReservaHotel.Tests/Core/ReservationFixture.cs
@@ -0,0 +1,19 @@
+﻿namespace ReservaHotel.Tests.Core;
+
+public class ReservationFixture
+{
+    public Reservation MakeReservation()
+    {
+        DateTime start = DateTime.Now;
+        DateTime end = DateTime.Parse("02/06/2023");
+
+        var faker = new Faker<Reservation>()
+            .RuleFor(b => b.reservationId, f => f.IndexFaker)
+            .RuleFor(b => b.data_init, start)
+            .RuleFor(b => b.data_finish, end)
+            .RuleFor(b => b.numberAdults, f => f.Random.Number(1,4))
+            .RuleFor(b => b.numberChildren, f => f.Random.Number(1,4));
+
+        return faker.Generate();
+    }
+}
---
+++ tests/ReservaHotel.Tests/Domain/ReservationTests.cs
@@ -0,0 +1,32 @@
+﻿using ReservaHotel.Tests.Core;
+using Xunit.Abstractions;
+
+namespace ReservaHotel.Tests.Domain;
+
+public class ReservationTests : IClassFixture<ReservationFixture>
+{
+    private readonly ReservationFixture _fixtureReservation;
+    private readonly RoomFixture _fixtureRoom;
+    private readonly ITestOutputHelper _output;
+
+    public ReservationTests(ReservationFixture fixtureReservation, ITestOutputHelper output, RoomFixture fixtureRoom)
+    {
+        _fixtureReservation = fixtureReservation;
+        _output = output;
+        _fixtureRoom = fixtureRoom;
+    }
+
+    [Fact]
+    public void ShouldCreateReservation()
+    {
+        var room = _fixtureRoom.AddNewRoom();
+        var reservation = _fixtureReservation.MakeReservation();
+
+        _output.WriteLine(reservation.ToString());
+        _output.WriteLine(room.ToString());
+
+        reservation.Should()
+            .NotBeNull();
+
+    }
+}
